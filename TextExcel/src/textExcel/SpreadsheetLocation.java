171aa6844712657f8d32fce3f36b261c29cce206
harryLaw14 <17lawh76@millshighsch.org>
2017-03-15 10:45:17 -0700

    checkpoint 2

--- TextExcel/src/textExcel/SpreadsheetLocation.java
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -1,23 +1,22 @@
 package textExcel;
 
+public class SpreadsheetLocation implements Location {
+    private int row;
+    private int column;
+  
+    public int getRow(){
+        return row;
+    }
 
-public class SpreadsheetLocation implements Location{
-	
-	private String coordinates = "";
-	
-	public SpreadsheetLocation(String cellName){
-		coordinates = cellName;
-  }
-	
-  @Override
-  public int getRow(){
-  	int rowNum = Integer.parseInt(coordinates.substring(1)) - 1;
-      return rowNum;
-  }
+    @Override
+    public int getCol(){
+        return column;
+    }
+  
+    public SpreadsheetLocation(String cellName){
+    	char temp=cellName.charAt(0);
+    	column=temp-'A';
+  		row=Integer.parseInt(cellName.substring(1))-1;
+    }
 
-  @Override
-  public int getCol(){
-  	int colNum = (int) (coordinates.charAt(0) - 65);
-  	return colNum;
-  }
 }
