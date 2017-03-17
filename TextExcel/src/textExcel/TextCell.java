3a4ed1d74ab3035151d16dbfe9aa2eb0ec100249
harryLaw14 <17lawh76@millshighsch.org>
2017-03-17 09:08:42 -0700

    need formula cell

--- TextExcel/src/textExcel/TextCell.java
+++ TextExcel/src/textExcel/TextCell.java
@@ -1,5 +1,23 @@
 package textExcel;
 
-public class TextCell {
+public class TextCell implements Cell {
+	private String content;
+	public TextCell(String initial){
+		content=initial;
+	}
+	public String abbreviatedCellText(){
+		String fix="";
+		if(content.length()>1){
+			fix=content.substring(1, content.length()-1);
+		}
+		fix+="                    ";
+		return fix.substring(0,10);
+	}
+
+	@Override
+	public String fullCellText() {
+		return content;
+	}
 
 }
+
---
+++ TextExcel/src/textExcel/ValueCell.java
@@ -0,0 +1,30 @@
+package textExcel;
+
+public class ValueCell extends RealCell {
+	public ValueCell(String enter){
+		super(enter);
+	}
+	
+	public double getDoubleValue(){
+		String ball=getCont();
+		double kkkkkk=Double.parseDouble(ball);
+		return kkkkkk;
+	}
+	
+	public String abbreviatedCellText(){
+		String ball=getCont();		
+		double kkkkkk=Double.parseDouble(ball);
+		ball=kkkkkk+"";
+		if(ball.indexOf(".")==-1){
+			ball+=(".0");
+		}
+		ball+="               ";
+		return ball.substring(0,10);
+	}
+	
+	public String fullTextCell(){
+		String ball=getCont();
+		
+		return  getDoubleValue()+"";
+	}
+}
